bc862a3df04eefa060cb3460211067224fca6758
Will <lijinzhou00@Outlook.com>
2022-10-09 16:18:57 +0800

    git test

--- src/Test/testGit.java
+++ src/Test/testGit.java
@@ -9,5 +9,7 @@ package Test;
 public class testGit {
     public static void main(String[] args) {
         System.out.println("Git test!!!");
+        System.out.println("Git test again!!!");
+
     }
 }
