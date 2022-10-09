4a9739630f87e278ede804de273e726ceeace0c3
Will <lijinzhou00@Outlook.com>
2022-10-09 16:16:35 +0800

    git test

---
+++ src/Test/testGit.java
@@ -0,0 +1,13 @@
+package Test;
+
+/**
+ * @author Will
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/10/9 16:09
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("Git test!!!");
+    }
+}
